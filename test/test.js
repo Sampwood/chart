b27522e7cc960eafb9cf2adf0701b936b7e28c23
Sam <zhicheng.chai@nokia.com>
2017-02-20 18:32:46 +0800

    add demo js for code coverage statistics

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var expect = require('chai').expect;
+var add = require('../js/demo')
 
 describe('#test', function() {
 	it('should be right', function() {
-		var result = 1 + 1;
+		var result = add(1, 1);
 		expect(result).to.equal(2);
 	})
 });
