82da9259c98a68b211c26d17910ac4ced70d1764
Sam <zhicheng.chai@nokia.com>
2017-02-20 15:52:31 +0800

    add demo test

--- test/test.js
+++ test/test.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var expect = require('chai').expect;
+
+describe('#test', function() {
+	it('should be right', function() {
+		var result = 1 + 1;
+		expect(result).to.equal(2);
+	})
+});
